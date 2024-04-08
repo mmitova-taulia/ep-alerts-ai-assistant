5a2e090f86d6d81144be74ef3a6a7222d7d79e13
Maya Mitova <maya.mitova@taulia.com>
2024-04-08 15:35:14 +0300

    Initial commit

---
+++ src/main/groovy/com/taulia/hackathon24/epalertsaiassistant/EpAlertsAiAssistantApplication.groovy
@@ -0,0 +1,13 @@
+package com.taulia.hackathon24.epalertsaiassistant
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class EpAlertsAiAssistantApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(EpAlertsAiAssistantApplication, args)
+  }
+
+}
