874b05cd9fb955d03689aec3efd9ce0418a25b27
Maya Mitova <maya.mitova@taulia.com>
2024-04-08 15:35:14 +0300

    Initial commit
    (cherry picked from commit 5a2e090f86d6d81144be74ef3a6a7222d7d79e13)

---
+++ src/test/groovy/com/taulia/hackathon24/epalertsaiassistant/EpAlertsAiAssistantApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.taulia.hackathon24.epalertsaiassistant
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EpAlertsAiAssistantApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
