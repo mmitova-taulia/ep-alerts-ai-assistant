1becf88efb0578bed9514c5d8ad000c65e8c6ea2
Maya Mitova <maya.mitova@taulia.com>
2024-04-09 09:15:19 +0300

    Implemented prompt function resolve_funder_id

--- src/main/groovy/com/taulia/hackathon24/epalertsaiassistant/service/EpAlertAiAssistantChatService.groovy
+++ src/main/groovy/com/taulia/hackathon24/epalertsaiassistant/service/EpAlertAiAssistantChatService.groovy
@@ -3,9 +3,14 @@ package com.taulia.hackathon24.epalertsaiassistant.service
 import org.springframework.ai.chat.messages.UserMessage
 import org.springframework.ai.chat.prompt.Prompt
 import org.springframework.ai.chat.prompt.SystemPromptTemplate
+import org.springframework.ai.model.function.FunctionCallback
+import org.springframework.ai.model.function.FunctionCallbackWrapper
 import org.springframework.ai.openai.OpenAiChatClient
+import org.springframework.ai.openai.OpenAiChatOptions
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.core.io.Resource
 import org.springframework.stereotype.Service
 
@@ -19,10 +24,29 @@ class EpAlertAiAssistantChatService {
   OpenAiChatClient chatClient
 
   String testSystemPromptForMessage(String message) {
+    OpenAiChatOptions promptOptions = OpenAiChatOptions.builder()
+      .withFunction("resolve_funder_id")
+      .build()
+
     UserMessage userMessage = new UserMessage(message)
     SystemPromptTemplate promptTemplate = new SystemPromptTemplate(systemPromptResource)
-    Prompt prompt = new Prompt([promptTemplate.createMessage(), userMessage])
+    Prompt prompt = new Prompt([promptTemplate.createMessage(), userMessage], promptOptions)
     chatClient.call(prompt).result.output.content
+  }
+
+  @Configuration
+  static class Config {
+
+    @Bean
+    FunctionCallback resolveFunderIdFunctionInfo() {
+
+      return FunctionCallbackWrapper.builder(new AlertsInformationService())
+        .withName("resolve_funder_id")
+        .withDescription("Resolves funderId by funderName")
+        .withResponseConverter((Response response) -> response.funderId())
+        .build();
+    }
+
   }
 
 }
